001fbc7d143adf8ec94ec8f28bcb1f5493fa2db7
mariyanaIM <mm15916826@edu1.pgzaimov.com>
2024-03-08 10:19:56 +0200

    Dobavqne na administrator

--- CoffeeANDTea-0a03fd2ca1e23383a30a449f583493fc09e7200e/Program.cs
+++ CoffeeANDTea-0a03fd2ca1e23383a30a449f583493fc09e7200e/Program.cs
@@ -1,3 +1,4 @@
+using ASPShopBag.Services;
 using CoffeeANDTea.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -10,7 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<Client>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<Client>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
 .AddEntityFrameworkStores<ApplicationDbContext>()
  .AddDefaultTokenProviders();
@@ -30,7 +31,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.PrepareDataBase().Wait();// !!! my service
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
