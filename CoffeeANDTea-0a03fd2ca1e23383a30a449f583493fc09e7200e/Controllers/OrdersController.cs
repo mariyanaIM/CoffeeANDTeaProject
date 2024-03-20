3c1e7815c811692d5f6a2efd96ba5c65abad98fd
mariyanaIM <mm15916826@edu1.pgzaimov.com>
2024-03-20 09:23:34 +0200

    Dobavqne na nerabotesht order kontroler

---
+++ CoffeeANDTea-0a03fd2ca1e23383a30a449f583493fc09e7200e/Controllers/OrdersController.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CoffeeANDTea.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CoffeeANDTea.Controllers
+{
+    [Authorize]
+    public class OrdersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<Client> _userManager;
+
+        public OrdersController(ApplicationDbContext context, UserManager<Client> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Orders
+        public async Task<IActionResult> Index()
+        {
+            if (User.IsInRole("Admin"))
+            {
+                var borsaDbContext = _context.Orders
+                                    .Include(o => o.Clients)
+                                    .Include(o => o.Drinks);
+                return View(await borsaDbContext.ToListAsync());
+            }
+            else
+            {
+                var borsaDbContext = _context.Orders
+                                    .Include(o => o.Clients)
+                                    .Include(o => o.Drinks)
+                                    .Where(x => x.ClientsId == _userManager.GetUserId(User));
+                return View(await borsaDbContext.ToListAsync());
+            }
+
+        }
+
+        // GET: Orders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.Clients)
+                .Include(o => o.Drinks)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // GET: Orders/Create
+        public IActionResult Create()
+        {
+            //ViewData["ClientsId"] = new SelectList(_context.Users, "Id", "Id");
+            ViewData["DrinksId"] = new SelectList(_context.Drinks, "Id", "Id");
+            return View();
+        }
+
+        // POST: Orders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DrinksId,Quantity,DateUpdate")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                order.ClientsId = _userManager.GetUserId(User);
+                order.DateUpdate = DateTime.Now;
+                _context.Orders.Add(order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            //ViewData["ClientsId"] = new SelectList(_context.Users, "Id", "Id", order.ClientsId);
+            ViewData["DrinksId"] = new SelectList(_context.Drinks, "Id", "Id", order.DrinksId);
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            //ViewData["ClientsId"] = new SelectList(_context.Users, "Id", "Id", order.ClientsId);
+            ViewData["DrinksId"] = new SelectList(_context.Drinks, "Id", "Id", order.DrinksId);
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DrinksId,Quantity,DateUpdate")] Order order)
+        {
+            if (id != order.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    order.ClientsId= _userManager.GetUserId(User);
+                    order.DateUpdate= DateTime.Now;
+                    _context.Orders.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            //ViewData["ClientsId"] = new SelectList(_context.Users, "Id", "Id", order.ClientsId);
+            ViewData["DrinksId"] = new SelectList(_context.Drinks, "Id", "Id", order.DrinksId);
+            return View(order);
+        }
+
+        // GET: Orders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.Clients)
+                .Include(o => o.Drinks)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order != null)
+            {
+                _context.Orders.Remove(order);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.Id == id);
+        }
+    }
+}
